bc4189784767fcbf658a9b910e4a8947a42c61ab
DanielRasp02 <qcda1@hotmail.com>
2025-01-25 13:25:23 -0500

    Correction commentaires.

--- classic.py
+++ classic.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 #
 '''
-Simple program to extract Midnite Classic solar charge controller data using classic_modbusdecoder.py module.
+Simple program to extract Midnite Classic solar charge controller data
+using classic_modbusdecoder.py module.
 
 Based on the following Github repository:
 
-https://github.com/ClassicDIY/ClassicMQTT/blob/master/code/Python/support/classic_modbusdecoder.py
+https://github.com/ClassicDIY/ClassicMQTT
 
 '''
 
@@ -13,12 +14,14 @@ from classic_modbusdecoder import getModbusData
 import pprint  
 import logging
 
-
+# Uncomment the following to see debug data as the program runs
 '''logging.basicConfig(
     level=logging.DEBUG,
     format="%(levelname)-5s %(module)s:%(lineno)s %(message)s",
 )'''
 
+# The function only need the IP address of the solar charge controller and
+# return a dict will all decoded register values and their name as keyes.
 data = {}
 for IP in ('192.168.20.10', '192.168.20.12'):
     print("\n\n=================================================================================")
