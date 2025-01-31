0c963a954bbb707630b6d1cf6ed5a7046e479dd7
DanielRasp02 <qcda1@hotmail.com>
2025-01-31 17:39:22 -0500

    New file

---
+++ dload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+REPO="https://raw.githubusercontent.com/qcda1/MidniteClassic/refs/heads/main/"
+FILES=("classic_modbusdecoder.py" "Payload.py")  # Liste des fichiers à télécharger
+
+for file in "${FILES[@]}"; do
+    if [ -f "$file" ]; then
+        # Supprimer l'ancien backup s'il existe
+        [ -f "$file.bak" ] && rm "$file.bak"
+        
+        # Renommer l'ancien fichier en .bak pour conserver la date/heure
+        mv "$file" "$file.bak"
+    fi
+    
+    echo "Downloading $file..."
+    curl -o "$file" "$REPO/$file"
+done
+
+echo "Done !"
